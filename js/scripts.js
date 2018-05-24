efa1e6d302b1ccf3030dc10e3bd8c450d7b1a29b
Greg kearns <kernzerly@me.com>
2018-05-23 21:09:59 -0700

    var let const

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+var myVeryFavoriteFoodName = "Pizza";
+/*function(yes) [
+  alert(yes);
+}();
+*/
+console.log(myVeryFavoriteFoodName);
+
+var myName = "Ryker";
+var myAge = "21";
+var gwIsTheBest = true;
+var gwIsTheBest = false;
+
+var posibleNull = 2 / 0
+console.log(possibleNull);
+
+gwIsTheBest = true
+console.log(gwIsTheBest);
+/*
+function (yes) {
+*/
+var dozen = 12;
+{
+  var dozen = 13;
+}
+console.log(dozen);
+
+console.log(classroom);
+var clasroom = 2;
+
+var myName = "greg";
+let myName = "greg";
+const myGoal = "java script";
+consol.log(myName, myGoal);
