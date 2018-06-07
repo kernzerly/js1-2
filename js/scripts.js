5a62f9d84701325b9c10253d51fdf95f99216793
Greg kearns <kernzerly@me.com>
2018-06-06 19:57:50 -0700

    day 4

--- js/scripts.js
+++ js/scripts.js
@@ -49,19 +49,97 @@
 
 // 2
 
-for(let i =0; i <= 3; i++){
-  console.log()
+// let favMorie = ["Empire Strikes Back", "Darth Vader"];
+// let secondFave = ["Return of the king", "Bill the Pony"];
+// let thirdFav = ["The room", "Lisa"];
+// let favoriteMovies = [];
+//
+// let products = [["b;amlet', [12.99], "The best blanket around]]
+// let i = 0;  //control statement
+// while (i < products.length) { //parameters}
+// while  (i < 3) { //parameters
+//   console.log(i, 'inside the loop');
+//   console.log(products[o]);
+//
+//   i++; //Incrementor
+//
+// }
+// // for(let i = 0; i < products.length; i++) {
+// //   console.log(products[i]);
+//   // var reversed = products.reverse();
+//
+// //}
+// let i = 0;
+// let newProduct = ["Sqeeky toy", 19.99, "Really good toy."];
+// do [
+//   //add newproduct to the second member of product Arrayi++;
+// } while(i <products.length);
+// products.push('');
+// if(i===1) products[i].push(newProduct);//add newproduct to the seconnd member of products array
+// i++;
+// } while(i < products.length);
+// console.log(products);
+//
+// product.forEach((product)=>)
+//
+//
+// for(let i =0; i <= 3; i++){
+//   console.log()
+// }
+// while(i < 3){
+//   console.log(i);
+// }
+// let numbers = [1,2,3];
+// numbers.forEach(function)(number){
+//   console.log(number);
+//   let newTask = document.createElement("li")
+// });
+// let task1 = 1;
+// let task2 = 2;
+// let task3 = 3;
+// if(task1 == 1 && task2 == 2 && task3 == 3)
+// console.log("JS homework" + "startup solar" + "demo spa");
+//
+// for(let i = products.length - 1 ;i >= 0; i--) {
+//   console.log(products[i]);
+//   // var reversed = products.reverse();
+//
+// }
+//Functions
+// console.log("Hello World");
+
+// function capitalCase(string){ //function definition
+//   let words = string.split("");
+//   words.forEach(word => {
+//     let letters = word.split('');
+//     let capped = letters[0].toUppercase();
+//     letters.splice(0, 1, capped);
+//     // word.split('')[0].toUpperCase();
+//
+//   });
+//
+//   return string.toUpperCase();
+// };
+// let newString = capitalCase("peter piper"); //function Call
+// console.log(newString);
+// let capsName = capitalCase("peter piper");
+// // array.pop();
+// function myCool(){}
+
+function dogYears(a) {
+  alert(a*7)
 }
-while(i < 3){
-  console.log(i);
+dogYears(2);
+// console.log(dogYears(2))
+
+// multiply(5); // 5
+
+function supplyForLife(age,product) {
+  let death = 80;
+  let timeLeft = death - age;
+  let daysLeft = 365 * timeLeft;
+  let allProduct = daysLeft * product;
+  alert(`you will need ${allProduct} to last you to ${death}`)
 }
-let numbers = [1,2,3];
-numbers.forEach(function)(number){
-  console.log(number);
-  let newTask = document.createElement("li")
-});
-let task1 = 1;
-let task2 = 2;
-let task3 = 3;
-if(task1 == 1 && task2 == 2 && task3 == 3)
-console.log("JS homework" + "startup solar" + "demo spa");
+
+supplyForLife(23,1)
