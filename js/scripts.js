c9ea043914a61f1c039e321b4907a4760c959db9
Greg kearns <kernzerly@me.com>
2018-06-13 19:15:46 -0700

    day6

--- js/scripts.js
+++ js/scripts.js
@@ -124,114 +124,175 @@
 // console.log(newString);
 // let capsName = capitalCase("peter piper");
 // // array.pop();
-// function myCool(){}
+// // function myCool(){}
+//
+// function dogYears(a) {
+//   alert(a*7)
+// }
+// dogYears(2);
+// // console.log(dogYears(2))
+//
+// // multiply(5); // 5
+//
+// function supplyForLife(age,product) {
+//   let death = 80;
+//   let timeLeft = death - age;
+//   let daysLeft = 365 * timeLeft;
+//   let allProduct = daysLeft * product;
+//   alert(`you will need ${allProduct} to last you to ${death}`)
+// }
+//
+// supplyForLife(23,1)
+// //creates an array of things you need or want to do this
+// let todo1 = {prompt("what do you want to dot first"
+// "eat"?)}
+// let todo2 = [prompt("wht do you want to do second?",
+// "eat")]
+// let todo3 = [prompt("what do you want to do third?",
+// "eat")];
+// //this is a goo place for edge casing
+// todos.push(todo1, todo2, todo3);
+// todos.push(todo2)  // more time
+// //step 2
+// for(let i = 0; i < todos.length; i++) {
+//   let timeline = prompt(`how many days will it take to get $(todos[i]} done?` , 2);
+//   timeline =
+//   !isNaN(parseInt(timeline)) //inside terinary
+//    ?
+//   parseInt(timeLine)
+//   :
+//   2 ; //didactic error checking
+//   todos[i].unshift(timeLine);
+// }//console.log(todos, 'after adding time it will take');
+// todos.sort();
+// console.log(todos, 'after sort');
+// console.lot(todos, 'after adding time it will take');
+// //using a while loop, find the task that will take the longest and log it to the console
+// let longest = todos[0];
+// let j = 0;
+// while(j < todos.length) [
+// if(todos[j][0] > longest[0] longest = todos[j];
+// //valid iline if statement
+// //longest = todos[j][0] > longest
+//  j++;
+// }
+// longest.push('this');
+//
+//  //using a do while loop add the strings that will take the longest
+//  let z = 0;
+//  do {
+//    if(todos[z][todos[z].length - 1]
+//       !== "this") {
+//         todos[z].push('done');
+//    }
+//    z++;
+//  }while(z < todos.length);
+//  let doneItems = [];
+//  todos.forEach(
+//    todo => {
+//      if(todo[todo.length - 1] === 'done')
+//      doneItems.push(todo);
+//    }
+//  );
+// alert(doneItems);
+//
+// //try it out #2
+// function lifetimeCalculator(age, dailyUse) {
+//   if(!isNaN(age) && !isNan(dailyUse)) {
+//     const deat = 80;
+//     retern (deat - age) * (dailyUse * 365);
+//   } else {
+//     userAge = prompt('how old are you?', '30');
+//     userUse = prompt("How much do you use every day?",
+// '1');
+// let userAge = lifetimeCalculator(parseInt(userAge),
+// parseInt(userUse)
+//
+//
+//   }
+//   }
+// }
+// //on your takehome-day5 branch write a program that uses functions to:
+// function evaNum(num){
+//   let num = prompt('Please enter a number');
+//   if ((!isNan(num)) {
+//     //take a number and return the square of that number (power of 2)
+//     alert(num*num);
+//   } else {
+//     //if a non-number argument is passed into the function, alert NaN and prompt for another response
+//     alert(NaN);
+//     evalnum();
+//   }
+// };
+// evalNum();
+// //In a second function, capitalize the first letter of a string and add a period (.) to the end of the string if it doesn't already end with a period
+// function capital() {
+//   let answer = prompt("Please enter a word");
+//   let firstLetter = answer.charAt(0).toUpperCase();
+//   let restOfWord = answer.slice(1);
+//   let cap = (firstLetter + restOfWord).toString();
+//   console.log(cap);
+// }
+//
+// capital()
 
-function dogYears(a) {
-  alert(a*7)
-}
-dogYears(2);
-// console.log(dogYears(2))
-
-// multiply(5); // 5
-
-function supplyForLife(age,product) {
-  let death = 80;
-  let timeLeft = death - age;
-  let daysLeft = 365 * timeLeft;
-  let allProduct = daysLeft * product;
-  alert(`you will need ${allProduct} to last you to ${death}`)
-}
-
-supplyForLife(23,1)
-//creates an array of things you need or want to do this
-let todo1 = {prompt("what do you want to dot first"
-"eat"?)}
-let todo2 = [prompt("wht do you want to do second?",
-"eat")]
-let todo3 = [prompt("what do you want to do third?",
-"eat")];
-//this is a goo place for edge casing
-todos.push(todo1, todo2, todo3);
-todos.push(todo2)  // more time
-//step 2
-for(let i = 0; i < todos.length; i++) {
-  let timeline = prompt(`how many days will it take to get $(todos[i]} done?` , 2);
-  timeline =
-  !isNaN(parseInt(timeline)) //inside terinary
-   ?
-  parseInt(timeLine)
-  :
-  2 ; //didactic error checking
-  todos[i].unshift(timeLine);
-}//console.log(todos, 'after adding time it will take');
-todos.sort();
-console.log(todos, 'after sort');
-console.lot(todos, 'after adding time it will take');
-//using a while loop, find the task that will take the longest and log it to the console
-let longest = todos[0];
-let j = 0;
-while(j < todos.length) [
-if(todos[j][0] > longest[0] longest = todos[j];
-//valid iline if statement
-//longest = todos[j][0] > longest
- j++;
-}
-longest.push('this');
-
- //using a do while loop add the strings that will take the longest
- let z = 0;
- do {
-   if(todos[z][todos[z].length - 1]
-      !== "this") {
-        todos[z].push('done');
-   }
-   z++;
- }while(z < todos.length);
- let doneItems = [];
- todos.forEach(
-   todo => {
-     if(todo[todo.length - 1] === 'done')
-     doneItems.push(todo);
-   }
- );
-alert(doneItems);
-
-//try it out #2
-function lifetimeCalculator(age, dailyUse) {
-  if(!isNaN(age) && !isNan(dailyUse)) {
-    const deat = 80;
-    retern (deat - age) * (dailyUse * 365);
-  } else {
-    userAge = prompt('how old are you?', '30');
-    userUse = prompt("How much do you use every day?",
-'1');
-let userAge = lifetimeCalculator(parseInt(userAge),
-parseInt(userUse)
+let body =
+document.getElementsByTagName('body');
+console.log(body[0]);
+//Array
+let classes =
+document.getElementsByClassName("my-header");
+console.log(classes);
+let id =
+document.getElementById("my-paragraph");
+console.log(id);
+//id's should only be used for js not styling not best practice
+//newer methods:
+let tags = document.querySelector('body');
+console.log(tags);
+let myClass = document.querySelector('.my-header');
+console.log(myClass);  //class alone would turn purple
+let myId =
+document.querySelector('#my-paragraph');
+console.log(myId);
 
 
+function queryDom(methodName, element) {
+  if(methodName == className);{
+    if(methodName == tagName);
+    if(methodName == id);
   }
-  }
-}
-//on your takehome-day5 branch write a program that uses functions to:
-function evaNum(num){
-  let num = prompt('Please enter a number');
-  if ((!isNan(num)) {
-    //take a number and return the square of that number (power of 2)
-    alert(num*num);
-  } else {
-    //if a non-number argument is passed into the function, alert NaN and prompt for another response
-    alert(NaN);
-    evalnum();
-  }
-};
-evalNum();
-//In a second function, capitalize the first letter of a string and add a period (.) to the end of the string if it doesn't already end with a period
-function capital() {
-  let answer = prompt("Please enter a word");
-  let firstLetter = answer.charAt(0).toUpperCase();
-  let restOfWord = answer.slice(1);
-  let cap = (firstLetter + restOfWord).toString();
-  console.log(cap);
 }
 
-capital()
+//try it
+// function queryDom(methodName, element){
+//   swith(methodName.toLowerCase()){
+//     case 'getElemtnsByTagName':
+//       document.getElementsByTagName(element);
+//       break;
+//     case 'queryselector':
+//       document.querySelector(`.${element}`);
+//       break;
+//     default:
+//       break;
+//   }
+// }
+myClass.addEventListener('click',
+  e=> { alert("hi")})//callback functions  2nd  listening for events
+// day 6 try it
+let button= document.querySelector('button');
+let div = document.querySelector('div');
+let h1 = document.querySelector('h1');
+
+
+button.addEventListener('click',e=> {  //call back function
+  alert('you hit this button');
+})
+
+div.addEventListener('mouseenter', e=> {
+   e.target.style.backgroundColor = ('red');
+
+})
+h1.addEventListener('mouseleave', e => {
+   e.target.style.color = ('red');
+})
