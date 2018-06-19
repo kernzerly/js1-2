4fca9d76838ec06e7af6531b38163d2d0935c1e3
Greg kearns <kernzerly@me.com>
2018-06-18 18:03:54 -0700

    day7

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,26 @@
+function createRow() {
+  const row = document.createElement('div');
+  row.classList.add('row');
+  for(let i = 5; i > 0; i--) {
+    let column = document.createElement('div')
+    column.classList.add('col-md-2');
+    column.classList.add('column');
+//user input element
+// dom manipulation
+//  net ninja
+// traversy media add item lister changing style individual li's
+    row.appendChild(column);
+  }
+  return row;
+}
 
+let bob = createRow();
+console.log(bob);
+
+<input type="button" value="Click Me">
+
+document.getElementById("myBtn").addEventListener("click", displayDate);
+
+function displayDate() {
+    document.getElementById("demo").innerHTML = Date();
+}
